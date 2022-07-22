5ed8737666ee2fff32fc4f58b2ffcfedb9dba865
Yiit <imyiit@outlook.com>
2022-07-22 19:14:51 +0300

    changed some

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,8 @@ interface Options {
   minify: boolean;
 }
 
+type data = string | Object | Array | number | boolean;
+
 declare class Sags {
   constructor(options: Options);
 
@@ -33,7 +35,7 @@ declare class Sags {
   /**
    * Database kb size
    */
-  dbSize(): number
+  dbSize(): number;
   /**
    * Delete data
    * @param key Need for delete data
@@ -47,7 +49,7 @@ declare class Sags {
    * Get data
    * @param key Need for get data
    */
-  get(key: string): any;
+  get(key: string): data;
   /**
    * Checking database for this data
    * @param key Need for check has data
@@ -56,24 +58,24 @@ declare class Sags {
   /**
    * @param key Need for find head
    */
-  head(key: string): any;
+  head(key: string): data;
   /**
    * @param key Need for find element
    * @param index Index number
    */
-  nth(key: string, index: number): any;
+  nth(key: string, index: number): data;
   /**
    * Push data to array
    * @param key Need for push data
    * @param data Pushing item
    */
-  push(key: string, data: any): boolean;
+  push(key: string, data: data): boolean;
   /**
    * Set data
    * @param key Need for save data
    * @param data Saved data
    */
-  set(key: string, data: any): boolean;
+  set(key: string, data: data): boolean;
   /**
    * @param key Need for subtract number
    * @param num Number size
@@ -82,18 +84,18 @@ declare class Sags {
   /**
    * @param key Need for find tail
    */
-  tail(key: string): any;
+  tail(key: string): data;
   /**
    * Checking data type
    * @param key Need for check type of data
    */
-  type(key: string): any;
+  type(key: string): data;
   /**
    * Unpush an element from array
    * @param key Need for unpush data from array
    * @param data Unpushing item
    */
-  unpush(key: string, data: any): boolean;
+  unpush(key: string, data: data): boolean;
 }
 
 export = Sags;
