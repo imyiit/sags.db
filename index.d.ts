3068b0857de139a61b9c8e00a77bca160b2e5c36
Yiit <imyiit@outlook.com>
2022-07-22 16:19:57 +0300

    added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+export class Sags {
+  constructor({
+    name,
+    folder,
+    minify,
+  }: {
+    name: string;
+    folder: string;
+    minify: boolean;
+  });
+
+  set(key: string, data: any): boolean;
+  delete(key: string): boolean;
+  get(key: string): any;
+  type(key: string): type;
+  has(key: string): boolean;
+  all(): any;
+  deleteAll(): boolean;
+  push(key: string, data: any): boolean;
+  unpush(key: string, data: any): boolean;
+  add(key: string, num: number): boolean;
+  subtract(key: string, num: number): boolean;
+  head(key: string): any;
+  tail(key: string): any;
+  nth(key: string, index: number): any;
+  dbSize(): number;
+  
+}
